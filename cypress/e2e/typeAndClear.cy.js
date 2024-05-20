81384c62b516c100824de68f9324033981c096f7
Maciek R <maciej.rogala@dgsoftware.pl>
2024-05-20 12:04:22 +0200

    refactor

--- cypress/e2e/typeAndClear.cy.js
+++ cypress/e2e/typeAndClear.cy.js
@@ -4,8 +4,6 @@ describe('wpisywanie i czyszczenie', () => {
 	it('wpisywanie wartosci', () => {
 		cy.visit('/')
 		cy.get('#search_query_top').type('dress', { delay: 200 })
-	})
-	it('czyszczenie wartosci', () => {
 		cy.get('#search_query_top').clear()
 	})
 })
